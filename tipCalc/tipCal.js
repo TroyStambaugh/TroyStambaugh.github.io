e0c2fbab958eefd375b338610ca598c5b80337ca
TroyStambaugh <troystambaugh@WilliamStambaughMBP.local>
2016-09-29 12:40:31 -0400

    tipCal

--- tipCalc/tipCal.js
+++ tipCalc/tipCal.js
@@ -1,19 +1,21 @@
  var tip=0
  
-
+//sets var ts= tip service, bt= bill total, sb= split bill, tip= the tip amount
     function cal() {
          $("#tf").empty();
         var ts = $("#ts").val();
         var bt = $("#bt").val();
         var sb = $("#sb").val();
         var tip=(bt/sb) *ts;
+
+        //if left blank or text entered then will send alert
     if (bt== "" || bt == null) {
         alert("Enter bill total!");
         return false;}
         if (sb >=2){
             tip = (tip+(bt/sb));
         } 
-    
+    //tip field 
 $("#tf").append("$"+tip.toFixed(2));    
 }
 function redo() {
